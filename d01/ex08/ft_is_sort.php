6590f9481d9429af163a358de9dff80c3e581965
lsandor- <fedorizmalkov@gmail.com>
2019-05-01 12:09:49 +0300

    1

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,2 +1,13 @@
-#!/usr/bin/php
 <?PHP
+function ft_is_sort($array)
+{
+    $i = count($array);
+    if ($i == 0 || $i == 1)
+        return true;
+    for ($j = 1; $j < $i; $j++)
+    {
+        if ($array[$j - 1] > $array[$j])
+            return false;
+    }
+    return true;
+}
