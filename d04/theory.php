9781473cd28f7a362d7cc8c83aa095c5d5a35884
lsandor- <fedorizmalkov@gmail.com>
2019-05-03 23:45:10 +0300

    1

---
+++ d04/theory.php
@@ -0,0 +1,33 @@
+<?php
+// php hash manual
+//whirlpool
+session_start() creates a session or resumes the current one based on a session 
+identifier passed via a GET or POST request, or passed via a cookie.
+
+$_GET
+An associative array of variables passed to the current script via the 
+URL parameters (aka. query string). Note that the array is not only populated 
+for GET requests, but rather for all requests with a query string.
+
+$_SESSION
+
+curl -v, --verbose
+Makes  curl verbose during the operation. Useful for debugging and seeing what's 
+going on "under the hood". A line starting with '>' means "header data" sent by curl, 
+'<' means "header data" received by curl that is
+hidden in normal cases, and a line starting with '*' means additional info provided 
+by curl.
+
+-b, --cookie <data>
+(HTTP) Pass the data to the HTTP server in the Cookie header. It is supposedly the data
+ previously received from the server in a "Set-Cookie:" line.  The data should be in the 
+ format "NAME1=VALUE1; NAME2=VALUE2".
+
+If no '=' symbol is used in the argument, it is instead treated as a filename to read 
+previously stored cookie from. This option also activates the cookie engine which will
+ make curl record incoming  cookies,  which
+may be handy if you're using this in combination with the -L, --location option or do
+ multiple URL transfers on the same invoke.
+
+The file format of the file to read cookies from should be plain HTTP headers 
+(Set-Cookie style) or the Netscape/Mozilla cookie file format.
