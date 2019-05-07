f6b3db9e36558b18b779a72687dce24ec1b90e64
lsandor- <fedorizmalkov@gmail.com>
2019-05-07 20:23:22 +0300

    1

---
+++ d05/ex16/ex16.sql
@@ -0,0 +1,5 @@
+SELECT count(*) as movies
+FROM `db_lsandor-`.member_history
+WHERE `date` BETWEEN '2006-10-30' AND '2007-07-27'
+OR (MONTH(`date`) = '12'
+AND DAY(`date`) = '24');
---
+++ d05/ex19/ex19.sql
@@ -0,0 +1,2 @@
+SELECT DATEDIFF(MAX(`date_last_film`), MIN(`date_last_film`)) AS `uptime`
+FROM `db_lsandor-`.member;
