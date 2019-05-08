bc8b726c1f4ff37a4d2678b446c605155596b534
lsandor- <fedorizmalkov@gmail.com>
2019-05-08 23:10:41 +0300

    1

---
+++ d06/ex03/Color.class.php
@@ -0,0 +1,67 @@
+<?php
+class Color{
+	static $verbose = false;
+	public $red;
+	public $green;
+	public $blue;
+
+	public function __construct(array $arr)
+	{
+		if (isset($arr['rgb']))
+		{
+			$rgb = intval($arr['rgb']);
+			$this->red = (($rgb >> 16) & 255);
+			$this->green = (($rgb >> 8) & 255);
+			$this->blue = ($rgb & 255);
+		}
+		else
+		{
+			$this->red = intval($arr['red']);
+			$this->green = intval($arr['green']);
+			$this->blue = intval($arr['blue']);
+		}
+		if (self::$verbose === true)
+			echo $this->__toString()." constructed.".PHP_EOL;
+	}
+
+	public function __destruct()
+	{
+		if (self::$verbose === true)
+			echo $this->__toString()." destructed.".PHP_EOL;
+	}
+
+	public function __toString()
+	{
+		return sprintf("Color( red: %3d, green: %3d, blue: %3d )", $this->red, $this->green, $this->blue);
+	}
+
+	public static function doc()
+	{
+		return file_get_contents('Color.doc.txt');
+	}
+
+	public function add( Color $add )
+	{
+		$r = $this->red + $add->red;
+		$g = $this->green + $add->green;
+		$b = $this->blue + $add->blue;
+		return new Color(array('red' => $r, 'green' => $g, 'blue' => $b));
+	}
+
+	public function sub( Color $sub )
+	{
+		$r = $this->red - $sub->red;
+		$g = $this->green - $sub->green;
+		$b = $this->blue - $sub->blue;
+		return new Color(array('red' => $r, 'green' => $g, 'blue' => $b));
+	}
+
+	public function mult( $m )
+	{
+		$r = $this->red * $m;
+		$g = $this->green * $m;
+		$b = $this->blue * $m;
+		return new Color(array('red' => $r, 'green' => $g, 'blue' => $b));
+	}
+
+}
---
+++ d06/ex03/Vertex.class.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'Vertex.class.php';
+class Vertex{
+	private $_x;
+	private $_y;
+	private $_z;
+	private $_w;
+	private $_color;
+	static $verbose = false;
+	
+	public static function doc()
+	{
+		return file_get_contents('Vertex.doc.txt');
+	}
+
+	public function __construct(array $arr)
+	{
+			$this->_x = $arr['x'];
+			$this->_y = $arr['y'];
+			$this->_z = $arr['z'];
+
+			if (isset($arr['w']))
+				$this->_w = $arr['w'];
+			else
+				$this->_w = 1.0;
+			if (isset($arr['color']))
+				$this->_color = $arr['color'];
+			else
+				$this->_color = new Color(array('rgb' => 0xffffff));
+			if (self::$verbose === true)
+				echo $this->__toString()." constructed". PHP_EOL;
+	}
+
+	public function __destruct()
+	{
+		if (self::$verbose === true)
+			echo $this->__toString()." destructed". PHP_EOL;
+	}
+
+	public function __toString()
+	{
+		if (self::$verbose === true)
+			return sprintf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, %s )", $this->_x, $this->_y, $this->_z, $this->_w, $this->_color);
+		else
+			return sprintf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f )", $this->_x, $this->_y, $this->_z, $this->_w);
+	}
+
+	public function get_x(){
+		return $this->_x;
+	}
+
+	public function get_y(){
+		return $this->_y;
+	}
+
+	public function get_z(){
+		return $this->_z;
+	}
+
+	public function get_w(){
+		return $this->_w;
+	}
+
+	public function get_color(){
+		return $this->_color;
+	}
+
+	public function set_x($x){
+		$this->_x = $x;
+	}
+
+	public function set_y($y){
+		$this->_y = $y;
+	}
+
+	public function set_z($z){
+		$this->_z = $z;
+	}
+
+	public function set_w($w){
+		$this->_w = $w;
+	}
+
+	public function set_color(Color $color ){
+		$this->_color = $color;
+	}
+}
