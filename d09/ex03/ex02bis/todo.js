93ef53e0006062f9c0ee80ecaf07381c10d3c35b
lsandor- <fedorizmalkov@gmail.com>
2019-05-11 16:16:18 +0300

    1

--- d09/ex03/ex02bis/todo.js
+++ d09/ex03/ex02bis/todo.js
@@ -1,25 +1,27 @@
 var cookies = [];
 var ft_list;
 
-window.onload = function() {
-	document.querySelector("#new").addEventListener('click', newTodoList);
-	ft_list = document.querySelector('#ft_list');
+$(document).ready(function(){
+	$("#new").click(newTodoList)
+	$("#ft_list div").click(removeList);
+	ft_list = $('#ft_list');
 	var cookie = document.cookie.split(';');
+	console.log(cookie);
 	if (cookie){
 		cookies = cookie[1].split(',');
 		cookies.forEach(function (element){
 			addNewToDo(element);
 		});
 	}
-};
+});
 
-window.onunload = function(){
-	var todo = ft_list.children;
+$(window).on("beforeunload", function(){
+	var todo = ft_list.children();
 	var newCookies = [];
 	for (var i = 0; i < todo.length; i++)
 		newCookies.unshift(todo[i].innerHTML);
 	document.cookie = newCookies;
-};
+});
 
 function newTodoList(){
 	var newToDo = prompt("Fill in a new TO DO", "");
@@ -28,15 +30,12 @@ function newTodoList(){
 }
 
 function addNewToDo( list ){
-	var newBlock = document.createElement("div");
-	newBlock.innerHTML = list;
-	newBlock.addEventListener('click', removeList);
-	ft_list.insertBefore(newBlock, ft_list.firstChild);
+	ft_list.prepend($('<div>' + list + '</div>').click(removeList));
 }
 
 function removeList(){
 	if (confirm('Would you like to remove list?'))
 	{
-		this.parentElement.removeChild(this);
+		this.remove();
 	}
 }
